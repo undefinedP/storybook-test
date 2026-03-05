bd8080d98c8fecb39c9d23c8ef63f2c0c3f22026
undefinedP <kzbaram@gmail.com>
2026-03-05 13:23:43 +0900

    update workflow

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,13 @@
-
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
-
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
-    "@storybook/addon-designs"
+    "@storybook/addon-designs",
   ],
-  "framework": "@storybook/react-vite"
+  framework: "@storybook/react-vite",
 };
 export default config;
